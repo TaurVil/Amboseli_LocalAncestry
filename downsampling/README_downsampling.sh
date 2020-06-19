0b3a8e41e9ac68f5a1fbc6c6581cd3e7b325536b
TaurVil <41305538+TaurVil@users.noreply.github.com>
2020-06-19 13:04:42 -0500

    Update README_downsampling.sh

--- downsampling/README_downsampling.sh
+++ downsampling/README_downsampling.sh
@@ -10,4 +10,12 @@ for f in `cat 00_names.txt`; do sed -e s/FILENAME/$f/g run.calculate_coverage.sh
 ## Downsample to get 10x data, as well as 5x, 2x, 1x, and p5x. 
 cat *out | sed 's/Average =  //g' > 00_covs.txt
 sbatch --array=1-7 --mem=8G run.downsample.sh
+# check the output for coverage information
+for f in `cat 00_bams.txt`; do sed -e s/FILENAME/$f/g run.calculate_coverage.sh > tmp.sh; sbatch --mem=8G --out cov.$f.out tmp.sh; rm tmp.sh; done
 
+## Convert bams to chrXX rather than just XX
+for file in AMB*.bam; do filename=`echo $file | sed s/.bam//g`; samtools view -H $file | sed -e 's/SN:\([0-9XY]\)/SN:chr\1/' -e 's/SN:MT/SN:chrM/' | samtools reheader - $file > ${filename}_chr.bam; echo $file; echo ${filename}_chr.bam; done
+
+## Call genotypes for each set of bams
+mkdir gVCF; sbatch --array=1-35 --mem=16G  run.gvcf.sh
+sbatch --array=1-7 --mem=16G  run.gvcf_original.sh
