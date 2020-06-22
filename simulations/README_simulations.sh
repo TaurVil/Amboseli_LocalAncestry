4e555a668f9077a15f7aec2e136c6f539ccb751b
TaurVil <41305538+TaurVil@users.noreply.github.com>
2020-06-22 16:59:11 -0500

    Update README_simulations.sh

--- simulations/README_simulations.sh
+++ simulations/README_simulations.sh
@@ -33,12 +33,27 @@ zcat /data/tunglab/tpv/panubis1_genotypes/calls_unadmixed/02.yel.20.recode.vcf.g
 module load vcftools; cd /data/tunglab/tpv/panubis1_genotypes/calls_unadmixed; for f in `seq 17 20`; do vcftools --gzvcf 02.yel.$f.recode.vcf.gz --freq --out chr$f.yellow; sed -i '1d' chr$f.yellow.frq; sed -i 's/:/\t/g' chr$f.yellow.frq; vcftools --gzvcf 02.anu.$f.recode.vcf.gz --freq --out chr$f.anubis; sed -i '1d' chr$f.anubis.frq; sed -i 's/:/\t/g' chr$f.anubis.frq; done
 mv chr*frq /data/tunglab/tpv/local_ancestry/simulated_data/; cd /data/tunglab/tpv/local_ancestry/simulated_data/; 
 
-#Read in tracts
-#Read in anubis/yellow frequencies 
-####for f in `cat 00names`; do cat get_vcfs.R | sed -e s/NAME/$f/g  > g.$f.R; sbatch g.$f.R; done 
+# get a simplified vcf file for each sample
 mkdir simulated_vcfs; module load R; for f in `cat 00names`; do for g in `cat 00chroms`; do cat run.get_sample_vcf.R | sed -e s/NAME/$f/g | sed -e s/SCAF/$g/g > g.$f.$g.R; sbatch g.$f.$g.R; rm g.$f.$g.R; done; done 
 
-#Outputs vcf file for each individual 
+# We need to add the full vcf header to each sample
+zcat /data/tunglab/tpv/panubis1_genotypes/calls_merged/04.merged_shared.1.vcf.gz | grep '^#' | sed '$ d' > full_header.vcf
+for f in `cat 00names`; do cat full_header.vcf > simulated_vcfs/$f.vcf; head -1 simulated_vcfs/$f.chr20.vcf >> simulated_vcfs/$f.vcf; for g in `ls simulated_vcfs/$f.chr*vcf`; do sed '1d' $g >> simulated_vcfs/$f.vcf; done; done
+
+# Get small genome to generate reads for
+module load samtools; for f in `cat 00chroms`; do samtools faidx /data/tunglab/shared/genomes/panubis1/Panubis1.0.fa $f > $f.fa; echo $f; done; cat chr*.fa > Reduced_Genome.fa; rm chr*.fa
+
+##Simulate reads using NEAT-genreads, a tool developed by Zachary Stephens 
+#github: https://github.com/zstephens/neat-genreads
+#publication: https://www.ncbi.nlm.nih.gov/pmc/articles/PMC5125660/
+# Generate 10x coverage in SE, 100bp reads
+mkdir sim_reads
+for f in `cat 00names`; do sed s/INDIV/$f/g run.get_fasta.sh > g.$f.sh; sbatch g.$f.sh; done
+
+
+
+
+
 ##We need to modify these a bit to get only SNPs of interest and include a proper header  
 for f in `ls i*Super-Scaff*.vcf`; do sed '1d' $f > 2.$f; done 
 for f in `ls 2.i*`; do sed -e s/FILE/$f/g get_simple_vcf.R > g.$f.sh; sbatch g.$f.sh; done
