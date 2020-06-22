0449902febc709dce6f6f606560b1ab2a03383c6
TaurVil <41305538+TaurVil@users.noreply.github.com>
2020-06-22 12:02:58 -0500

    Update run.merge_gvcfs.sh

--- downsampling/run.merge_gvcfs.sh
+++ downsampling/run.merge_gvcfs.sh
@@ -8,9 +8,10 @@ module load java; module load samtools; module load python
 module load htslib
 module load GATK
 
-ls gVCF/*320*.$coverage*vcf.gz > 02.$coverage.list
+ls gVCF/*.$coverage*vcf.gz > 02.$coverage.list
 
 GenomeAnalysisTK.sh CombineGVCFs  -R $path_genome -O test.$coverage.g.vcf.gz -V 02.$coverage.list
 
 GenomeAnalysisTK.sh GenotypeGVCFs -R $path_genome -V test.$coverage.g.vcf.gz -O test.$coverage.vcf.gz --tmp-dir /data/tunglab/tpv/scratch/
 
+rm test.$coverage.g.vcf.gz; rm 02.$coverage.list; rm test.$coverage.g.vcf.gz.tbi
