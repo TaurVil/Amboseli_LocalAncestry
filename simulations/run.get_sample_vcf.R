8bc0f56f2b59265538bbd880bb10f3da9a7c9ce7
TaurVil <41305538+TaurVil@users.noreply.github.com>
2020-06-22 11:25:38 -0500

    Update run.get_sample_vcf.R

--- simulations/run.get_sample_vcf.R
+++ simulations/run.get_sample_vcf.R
@@ -55,8 +55,8 @@ get_gt <- function(site) {
 	return(genotype)
 }
 
-#library(parallel); detectCores() -> cores; c1 <- makeCluster(cores); clusterExport(c1, ls()); vcf[,10] <- do.call("c",parLapply(c1,sites,get_gt))
-
+library(parallel)
+#detectCores() -> cores; c1 <- makeCluster(cores); clusterExport(c1, ls()); vcf[,10] <- do.call("c",parLapply(c1,sites,get_gt))
 vcf[,10] <- do.call("c",mclapply(sites,get_gt))
 colnames(vcf)[10] <- "sampleNAME"
 
