fd0914342deed6b32fe04e6066412a32ff21382b
TaurVil <41305538+TaurVil@users.noreply.github.com>
2020-06-22 20:11:18 -0500

    Update README_simulations.sh

--- simulations/README_simulations.sh
+++ simulations/README_simulations.sh
@@ -36,29 +36,19 @@ mv chr*frq /data/tunglab/tpv/local_ancestry/simulated_data/; cd /data/tunglab/tp
 # get a simplified vcf file for each sample
 mkdir simulated_vcfs; module load R; for f in `cat 00names`; do for g in `cat 00chroms`; do cat run.get_sample_vcf.R | sed -e s/NAME/$f/g | sed -e s/SCAF/$g/g > g.$f.$g.R; sbatch g.$f.$g.R; rm g.$f.$g.R; done; done 
 
-# We need to add the full vcf header to each sample
+# We need to add the full vcf header to each sample, which we'll do with this and within the for loop below 
 zcat /data/tunglab/tpv/panubis1_genotypes/calls_merged/04.merged_shared.1.vcf.gz | grep '^#' | sed '$ d' > full_header.vcf
-for f in `cat 00names`; do cat full_header.vcf > simulated_vcfs/$f.vcf; head -1 simulated_vcfs/$f.chr20.vcf >> simulated_vcfs/$f.vcf; for g in `ls simulated_vcfs/$f.chr*vcf`; do sed '1d' $g >> simulated_vcfs/$f.vcf; done; done
 
-# Get small genome to generate reads for
-module load samtools; for f in `cat 00chroms`; do samtools faidx /data/tunglab/shared/genomes/panubis1/Panubis1.0.fa $f > $f.fa; echo $f; done; cat chr*.fa > Reduced_Genome.fa; rm chr*.fa
+# Get small genomes to generate reads for
+module load samtools; for f in `cat 00chroms`; do samtools faidx /data/tunglab/shared/genomes/panubis1/Panubis1.0.fa $f > $f.fa; echo $f; done; cat chr*.fa > Reduced_Genome.fa
 
 ##Simulate reads using NEAT-genreads, a tool developed by Zachary Stephens 
 #github: https://github.com/zstephens/neat-genreads
 #publication: https://www.ncbi.nlm.nih.gov/pmc/articles/PMC5125660/
 # Generate 10x coverage in SE, 100bp reads
 mkdir sim_reads
-for f in `cat 00names`; do sed s/INDIV/$f/g run.get_fasta.sh > g.$f.sh; sbatch g.$f.sh; done
+for f in `cat 00names`; do for g in `cat 00chroms`; do sed s/INDIV/$f/g run.get_fasta.sh | sed s/CHROMO/$g/g > g.$f.sh; sbatch g.$f.sh; rm g.$f.sh; done; done
 
 
-
-
-
-##We need to modify these a bit to get only SNPs of interest and include a proper header  
-for f in `ls i*Super-Scaff*.vcf`; do sed '1d' $f > 2.$f; done 
-for f in `ls 2.i*`; do sed -e s/FILE/$f/g get_simple_vcf.R > g.$f.sh; sbatch g.$f.sh; done
-
-rm 2.i*.vcf ; mv i*Super*.vcf True_VCFs/
-for f in `ls 3.2.*`; do cat header.vcf $f > temp; mv temp $f; echo $f; done 
 
 
