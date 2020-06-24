a046f62dcf548927067b515f797b6bd54a8a3992
TaurVil <41305538+TaurVil@users.noreply.github.com>
2020-06-24 12:51:43 -0500

    Update README_downsampling.sh

--- downsampling/README_downsampling.sh
+++ downsampling/README_downsampling.sh
@@ -2,7 +2,6 @@
 
 cd /data/tunglab/tpv/local_ancestry/downsampling
 
-
 ## Get coverage for each sample
 ls /data/tunglab/tpv/mapped_bams/amboseli_hicov/ | grep -v 'bai' | sed s/_MarkDuplicates.bam//g > 00_names.txt 
 for f in `cat 00_names.txt`; do sed -e s/FILENAME/$f/g run.calculate_coverage.sh > tmp.sh; sbatch --mem=8G --out cov.$f.out tmp.sh; rm tmp.sh; done
