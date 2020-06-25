acb366eb62ef74b6e00452b54f5bcefc0bf99ee0
TaurVil <41305538+TaurVil@users.noreply.github.com>
2020-06-25 18:18:00 -0500

    Update README_lclae.sh

--- unadmixed_samples/README_lclae.sh
+++ unadmixed_samples/README_lclae.sh
@@ -18,6 +18,10 @@ echo 21 `seq 42 63` > 02.yellow_minus1.h
 
 sbatch --array=44-62 run.get_raw_calls.sh
 
+## 63 is special and needs to be done manually
+
+for f in `echo 42`; do sed -e "s/ $f / /g" 02.yellow_minus1.h > $f.h; name=`head -$f ../refpanel.012.indv | tail -1`; for chrom in `seq 1 20`; do touch ./raw_calls/full_refpanel.35kb.p2.$f.$chrom.txt; l=`wc -l raw_calls/full_refpanel.35kb.p2.$f.$chrom.txt | cut -f 2 | sed 's/ .*//g'`; if (($l==0)); then echo "STARTING"; cat genolik.$chrom.genolik | /data/tunglab/tpv/Programs/LCLAE/filtbaboon2c 64 02.anubis.h $f.h $f | /data/tunglab/tpv/Programs/LCLAE/geno_lik2 .2 35000 > ./raw_calls/full_refpanel.35kb.p2.$f.$chrom.txt; fi ; echo $chrom; done; echo $f; rm $f.h; done
+
 
 for f in `seq 42 63`; do sed -e "s/ $f / /g" 02.yellow_minus1.h > $f.h; name=`head -$f ../refpanel.012.indv | tail -1`; for chrom in `seq 1 20`; do cat genolik.$chrom.genolik | /data/tunglab/tpv/Programs/LCLAE/filtbaboon2c 64 02.anubis.h $f.h $f | /data/tunglab/tpv/Programs/LCLAE/geno_lik2 .2 35000 > ./raw_calls/full_refpanel.35kb.p2.$f.$chrom.txt; echo $chrom; done; echo $f; rm $f.h; done
 
