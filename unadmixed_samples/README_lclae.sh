2441118cd5871fada3846101a89265f653302120
TaurVil <41305538+TaurVil@users.noreply.github.com>
2020-06-25 22:51:19 -0500

    Update README_lclae.sh

--- unadmixed_samples/README_lclae.sh
+++ unadmixed_samples/README_lclae.sh
@@ -22,10 +22,9 @@ sbatch --array=44-62 run.get_raw_calls.sh
 f=63
 for chrom in `seq 11 15 `; do touch ./raw_calls/full_refpanel.35kb.p2.$f.$chrom.txt; l=`wc -l raw_calls/full_refpanel.35kb.p2.$f.$chrom.txt | cut -f 2 | sed 's/ .*//g'`; if (($l==0)); then echo "STARTING"; cat genolik.$chrom.genolik | /data/tunglab/tpv/Programs/LCLAE/filtbaboon2c 64 02.anubis.h $f.h $f | /data/tunglab/tpv/Programs/LCLAE/geno_lik2 .2 35000 > ./raw_calls/full_refpanel.35kb.p2.$f.$chrom.txt; fi ; echo $chrom; done
 
-for f in `echo 42`; do sed -e "s/ $f / /g" 02.yellow_minus1.h > $f.h; name=`head -$f ../refpanel.012.indv | tail -1`; for chrom in `seq 1 20`; do touch ./raw_calls/full_refpanel.35kb.p2.$f.$chrom.txt; l=`wc -l raw_calls/full_refpanel.35kb.p2.$f.$chrom.txt | cut -f 2 | sed 's/ .*//g'`; if (($l==0)); then echo "STARTING"; cat genolik.$chrom.genolik | /data/tunglab/tpv/Programs/LCLAE/filtbaboon2c 64 02.anubis.h $f.h $f | /data/tunglab/tpv/Programs/LCLAE/geno_lik2 .2 35000 > ./raw_calls/full_refpanel.35kb.p2.$f.$chrom.txt; fi ; echo $chrom; done; echo $f; rm $f.h; done
+## Add chrom and merge
+for id in `seq 42 63`; do name=`head -$id ../refpanel.012.indv | tail -1`; for chrom in `seq 1 20`; do sed -i 's/^/\t/' ./raw_calls/full_refpanel.35kb.p2.$id.$chrom.txt; sed -i s/^/$chrom/ ./raw_calls/full_refpanel.35kb.p2.$id.$chrom.txt; done; cat ./raw_calls/full_refpanel.35kb.p2.$id.* > ./raw_calls/yellow.full_refpanel.35kb.p2.$name.$cov.txt; done
 
-
-for f in `seq 42 63`; do sed -e "s/ $f / /g" 02.yellow_minus1.h > $f.h; name=`head -$f ../refpanel.012.indv | tail -1`; for chrom in `seq 1 20`; do cat genolik.$chrom.genolik | /data/tunglab/tpv/Programs/LCLAE/filtbaboon2c 64 02.anubis.h $f.h $f | /data/tunglab/tpv/Programs/LCLAE/geno_lik2 .2 35000 > ./raw_calls/full_refpanel.35kb.p2.$f.$chrom.txt; echo $chrom; done; echo $f; rm $f.h; done
-
-for f in `echo 43`; do sed -e "s/ $f / /g" 02.yellow_minus1.h > $f.h; name=`head -$f ../refpanel.012.indv | tail -1`; for chrom in `seq 1 20`; do cat genolik.$chrom.genolik | /data/tunglab/tpv/Programs/LCLAE/filtbaboon2c 64 02.anubis.h $f.h $f | /data/tunglab/tpv/Programs/LCLAE/geno_lik2 .2 35000 > ./raw_calls/full_refpanel.35kb.p2.$f.$chrom.txt; echo $chrom; done; echo $f; rm $f.h; done
+mkdir tracts
+module load R; for id in `seq 42 63`; do name=`head -$id ../refpanel.012.indv | tail -1`; sed -e s/INDIV/$name/g run.get_tracts.R > r.$id.R; sbatch r.$id.R; rm r.$id.R; done
 
