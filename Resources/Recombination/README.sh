5371e05a7fb078d915c9905ecd0c7abd2fc70b76
TaurVil <41305538+TaurVil@users.noreply.github.com>
2020-07-04 11:06:56 -0500

    Create README.sh

---
+++ Resources/Recombination/README.sh
@@ -0,0 +1,8 @@
+## Get recombination maps for the panubis1 genome
+# Uses genotype calls for anubis and yellow individuals from /Amboseli_LocalAncestry/Resouces/Genotype_Calls
+
+
+## Filter genotypes
+# Filtering instructions from Pfeifer 2020 (vervet monkey genetic map)
+
+# Remove fixed alleles
