e425c08d579a3b3586b2da3122d4df8a583c4c16
TaurVil <41305538+TaurVil@users.noreply.github.com>
2020-07-04 11:08:51 -0500

    Update README.sh

--- Resources/Recombination/README.sh
+++ Resources/Recombination/README.sh
@@ -1,4 +1,10 @@
 ## Get recombination maps for the panubis1 genome
+# We'll be making 4 maps:
+#        -anubis map (n=24 SW anubis)
+#        -yellow map (n=XX high-cov yellow)
+#        -SW yellow map
+#        -Mikumi yellow map
+#        -Downsampled anubis maps (to match partial yellow maps)
 # Uses genotype calls for anubis and yellow individuals from /Amboseli_LocalAncestry/Resouces/Genotype_Calls
 
 
