fe85a78ec1ef2c1dc19977b14151553f2e46e0e7
TaurVil <41305538+TaurVil@users.noreply.github.com>
2020-07-05 09:33:01 -0500

    Update README.sh

--- Resources/Genotype_Calls/README.sh
+++ Resources/Genotype_Calls/README.sh
@@ -7,8 +7,23 @@ sbatch --array=1-20 --mem=12G run.01.get_amboseli.sh
 ## Get set of mutual calls, and merged file
 sbatch --array=1-20 --mem=12G run.02.merge.sh
 
+## merge vcfs for yellow and anubis baboons
+module load bcftools
+bcftools concat /data/tunglab/tpv/panubis1_genotypes/calls_unadmixed/02.anu.*.vcf.gz -O z -o /data/tunglab/tpv/panubis1_genotypes/anubis.vcf.gz
+bcftools concat /data/tunglab/tpv/panubis1_genotypes/calls_unadmixed/02.yel.*.vcf.gz -O z -o /data/tunglab/tpv/panubis1_genotypes/yellow.vcf.gz
+
+## Estimate relatedness for unadmixed individuals using KING (http://people.virginia.edu/~wc9c/KING/manual.html)
+# convert to plink format
+plink --vcf hicov.amboseli.recode.vcf --maf 0.05 --recode --out plink.ambo 
+
+# Install KING
+##### wget http://people.virginia.edu/~wc9c/KING/Linux-king.tar.gz; tar -xvf Linux-king.tar.gz
+# run KING
+/data/tunglab/tpv/Programs/KING/king
+module load gcc; module load glibc/2.18-gcb01
 
 ## Let's get just a bit of individual and site level information
 
 module load vcftools
-vcftools --gzvcf calls_unadmixed/01b.gatk_filtered.unadmixed.20.vcf.gz --depth --out chr20_unadmixed
+vcftools --gzvcf ./anubis.vcf.gz --depth --out anubis
+vcftools --gzvcf ./yellow.vcf.gz --depth --out yellow
