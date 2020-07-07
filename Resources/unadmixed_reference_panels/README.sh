90c0d35dcadddfee4bce7850d86cec899e20964b
TaurVil <41305538+TaurVil@users.noreply.github.com>
2020-07-07 09:29:53 -0500

    Create README.sh

---
+++ Resources/unadmixed_reference_panels/README.sh
@@ -0,0 +1,14 @@
+### This folder houses files of individuals from each possible reference population. 
+
+## Anubis: n=41
+# SW: n=24 from SW NPRC founder individuals Robinson et al. 2019
+# Wall: n=13
+# Wall, Masai Mara: n=7 from Wall et al. 2016
+# Wall, Tulane NPRC: n=6 from Wall et al. 2016
+# Baboon Genome Project: n=4 from Rogers et al. 2019
+
+## Yellow: n=23
+# SW: n=7 from SW NPRC founder individuals in Robinson et al. 2019. 
+# Wall: n=15
+# Mikumi: n=14, 10 high coverage new sequencing & 4 low coverage from Wall et al. 2016
+# Baboon Genome Project: n=1 from Mikumi and Rogers et al. 2019
