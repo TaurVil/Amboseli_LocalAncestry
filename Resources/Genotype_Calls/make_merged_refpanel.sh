56bdc9a623ac5429a07c2b8ac580422c46e32727
TaurVil <41305538+TaurVil@users.noreply.github.com>
2020-07-07 09:58:11 -0500

    Update and rename make_merged_refpanel.sh to Resources/Genotype_Calls/make_merged_refpanel.sh

--- Resources/Genotype_Calls/make_merged_refpanel.sh
+++ Resources/Genotype_Calls/make_merged_refpanel.sh
@@ -1,4 +1,4 @@
-# Make merged refpanel of yellow and anubis samples, for all chromosomes
+# Merge refpanel of yellow and anubis samples, for all chromosomes. Add 'chr' to chromosome index
 
 module load bcftools
 
@@ -11,5 +11,6 @@ vcftools --gzvcf ../refpanel.vcf.gz --mac 2 --max-missing 0.5 --kept-sites --out
 ## Add "chr" to kept-sites file & vcf
 sed -e 's/^\([0-9XY]\)/chr\1/' ../refpanel.kept.sites > ../refpanel.kept_chroms.sites; mv ../refpanel.kept_chroms.sites ../refpanel.kept.sites
 
+## adjust both the file header and each individual row
 zcat ../refpanel.vcf.gz | sed -e 's/##contig=<ID=\([0-9XY]\)/##contig=<ID=chr\1/' -e 's/^\([0-9XY]\)/chr\1/' > ../tmp.vcf
 mv ../tmp.vcf ../refpanel.vcf; bgzip ../refpanel.vcf; tabix ../refpanel.vcf.gz
