3eab310dd7cb9c4878be668d85e8faf28e46d94d
TaurVil <41305538+TaurVil@users.noreply.github.com>
2020-07-24 09:37:27 -0500

    Update README_LCLAE.sh

--- 1b_HybridZoneStructure/unadmixed_individuals/README_LCLAE.sh
+++ 1b_HybridZoneStructure/unadmixed_individuals/README_LCLAE.sh
@@ -20,13 +20,17 @@ echo 41 `seq 1 41` > 02.anubis.h; echo 22 `seq 42 63` > 02.yellow.h
 echo 21 `seq 42 63` > 02.yellow_minus1.h; echo 40 `seq 1 41` > 02.anubis_minus1.h
 
 sbatch --array=42-62 run.get_raw_calls.sh
+sbatch --array=1-40 run.get_raw_calls2.sh
 
 ## 63 is special and the *.h file needs to be done manually
 f=63
 for chrom in `seq 1 20 `; do touch ./raw_calls/full_refpanel.35kb.p2.$f.$chrom.txt; l=`wc -l raw_calls/full_refpanel.35kb.p2.$f.$chrom.txt | cut -f 2 | sed 's/ .*//g'`; if (($l==0)); then echo "STARTING"; cat genolik.$chrom.genolik | /data/tunglab/tpv/Programs/LCLAE/filtbaboon2c 63 02.anubis.h $f.h $f | /data/tunglab/tpv/Programs/LCLAE/geno_lik2 .2 35000 > ./raw_calls/full_refpanel.35kb.p2.$f.$chrom.txt; fi ; echo $chrom; done
 
+f=41
+for chrom in `seq 1 20 `; do touch ./raw_calls/full_refpanel.35kb.p2.$f.$chrom.txt; l=`wc -l raw_calls/full_refpanel.35kb.p2.$f.$chrom.txt | cut -f 2 | sed 's/ .*//g'`; if (($l==0)); then echo "STARTING"; cat genolik.$chrom.genolik | /data/tunglab/tpv/Programs/LCLAE/filtbaboon2c 63 $f.h 02.yellow.h $f | /data/tunglab/tpv/Programs/LCLAE/geno_lik2 .2 35000 > ./raw_calls/full_refpanel.35kb.p2.$f.$chrom.txt; fi ; echo $chrom; done
+
 ## Add chrom and merge
-for id in `seq 42 63`; do name=`head -$id ../refpanel.012.indv | tail -1`; for chrom in `seq 1 20`; do sed -i 's/^/\t/' ./raw_calls/full_refpanel.35kb.p2.$id.$chrom.txt; sed -i s/^/$chrom/ ./raw_calls/full_refpanel.35kb.p2.$id.$chrom.txt; done; cat ./raw_calls/full_refpanel.35kb.p2.$id.* > ./raw_calls/yellow.full_refpanel.35kb.p2.$name.$cov.txt; done
+for id in `seq 1 63`; do name=`head -$id ./refpanel.012.indv | tail -1`; for chrom in `seq 1 20`; do sed -i 's/^/\t/' ./raw_calls/full_refpanel.35kb.p2.$id.$chrom.txt; sed -i s/^/$chrom/ ./raw_calls/full_refpanel.35kb.p2.$id.$chrom.txt; done; cat ./raw_calls/full_refpanel.35kb.p2.$id.* > ./raw_calls/refpanel.full_refpanel.35kb.p2.$name.$cov.txt; done
 
 mkdir tracts
 module load R; for id in `seq 42 63`; do name=`head -$id ../refpanel.012.indv | tail -1`; sed -e s/INDIV/$name/g run.get_tracts.R > r.$id.R; sbatch r.$id.R; rm r.$id.R; done
