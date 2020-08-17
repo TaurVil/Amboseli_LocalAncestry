03544d117d478bcbc239bd5d8df18e6af81933c8
TaurVil <41305538+TaurVil@users.noreply.github.com>
2020-08-17 16:26:52 -0500

    Update README.sh

--- Resources/Recombination/README.sh
+++ Resources/Recombination/README.sh
@@ -24,16 +24,24 @@ java -jar /data/tunglab/tpv/Programs/GenomeAnalysisTK.jar -T VariantFiltration -
 # Stringent filtering for false variants
 ## SNPs showing an excess of heterozygosity were removed. Specifically, a P-value for Hardy–Weinberg Equilibrium was calculated using the “—hardy” option in VCFtools v.0.1.13 (Danecek et al. 2011), and SNPs with P < 0.01 removed.
 module load vcftools; vcftools --hardy --gzvcf anubisSW_cluster.vcf.gz --out anubisSW
-
+module load R; R
+library(data.table); fread("anubisSW.hwe") -> hwe; subset(hwe, hwe$P_HWE < 0.01) -> hwe; dim(hwe); write.table(hwe[,1:2], "anubisSW_hwe_to_remove.sites", row.names=F, col.names=F, sep="\t", quote=F)
+vcftools --gzvcf anubisSW_cluster.vcf.gz --recode-INFO-all --remove-filtered-all --exclude-positions anubisSW_hwe_to_remove.sites --recode --out anubisSW_hwe
+bgzip anubisSW_hwe.recode.vcf ; tabix anubisSW_hwe.recode.vcf.gz
 ## SNPs that could be recipricolly lifted over with the human genome? We'll ignore this, assuming the quality of the baboon genome is sufficient. 
+## Remove fixed alleles: done earlier with the maf frequency
 
-# Remove fixed alleles
-
-
-
-
-## Phasing with beagle
+## Phase with beagle
+###### Should we do this iteratively because it assumes a constant recombination map? 
 ############ Should we ask about error/differences in imputation based on the sample size (i.e. full panel vs partial yellow panels?) I'm actually going to just assume that's accounted for in the downsampling... 
+module load java; \java -Xmx4g -jar /data/tunglab/tpv/Programs/beagle.08Jun17.d8b.jar gt=anubisSW_hwe.recode.vcf.gz out=anubisSW_beag ne=40000; tabix anubisSW_beag.vcf.gz
+
+# Break to individual chromosomes
+for f in `seq 1 20`; do vcftools --gzvcf anubisSW_beag.vcf.gz --chr $f --recode --out $f.anubisSW ; bgzip $f.anubisSW.recode.vcf; tabix $f.anubisSW.recode.vcf.gz; done
+# Get individual-vcfs for each haplotype
+for f in `seq 1 20`; do for g in `cat ../00_anu_SW.list`; do sed -e s/CHROM/$f/g run.01a.panubis1.sh | sed -e s/INDIV/$g/g > g.sh; sbatch g.sh; rm g.sh; done; done
+# Run ldhelmet
+for f in `seq 1 20`; do sed -e s/CHROM/$f/g run.02a.panubis1.sh > r.all.sh; sbatch --mem=65000 --nice r.all.sh; rm r.all.sh; done
 
 
 
