3cf602b83c50aa63b45f47b49ee49f65bb93e2e9
TaurVil <41305538+TaurVil@users.noreply.github.com>
2020-09-14 17:18:52 -0500

    Create extract_allele_frequencies_within_Amboseli.sh

---
+++ 1a_LocalAncestry/extract_allele_frequencies_within_Amboseli.sh
@@ -0,0 +1,38 @@
+#### masked vs unmasked refpanel 
+masking <- 'unmasked'
+
+#########   Chunk to get data       ###########
+### Recent vs historical
+load(paste("~/Baboon/Resources/Recently_admixed_samples_",masking,".RData", sep=""))
+
+a_data <- fread("~/Baboon/Paper1a_ancestrycalling/amboseli_tracts_fullrefpanel.txt")
+colnames(a_data) <- c("chr", "start", "end", "state", "length", "u_prev", "u_after", "name")
+a_data$length <- a_data$end - a_data$start
+
+## Get "historical" tracts with between 10 and 500kb of homozygous anubis ancestry
+t_hist_names <- ids$name[ids$historical == 'yes']
+hist_tracts <- subset(a_data, a_data$name %in% t_hist_names & a_data$length > 10000 & a_data$length < 500000 & a_data$state == 2)
+write.table(hist_tracts, "~/Baboon/Paper1a_ancestrycalling/historical_homozygous_ancestry.txt", row.names=F, col.names=T, sep="\t")
+
+## Get "recent" tracts with >500kb of homozygous anubis ancestry
+t_recent_names <- rownames(recent); t_recent_names <- ids$name[ids$sname %in% t_recent_names]
+recent_tracts <- subset(a_data, a_data$name %in% t_recent_names & a_data$length > 500000 & a_data$state == 2)
+write.table(recent_tracts, "~/Baboon/Paper1a_ancestrycalling/recent_homozygous_ancestry.txt", row.names=F, col.names=T, sep="\t")
+
+## Each of these sum to ~19x individuals across the genome (assuming 3Gb which is a slight overestimate)
+## Move to /data/tunglab/tpv/local_ancestry/
+sed -i 's/\r//g' ../../local_ancestry/historical_hybrids.txt #Remove windows line breaks
+sed -i 's/\r//g' ../../local_ancestry/recent_hybrids.txt
+
+## Setup new working home
+cd /data/tunglab/tpv/panubis1_genotypes/
+mkdir amboseli_homozygous_segments; cd amboseli_homozygous_segments/
+
+## Get bed file per individual of sites to maintain, and list of individuals for recent and historic
+for f in `cat ../../local_ancestry/recent_hybrids.txt ../../local_ancestry/historical_hybrids.txt`; do grep $f ../../local_ancestry/amboseli_tracts_unmasked.txt > $f.homozygous.bed; done
+## Remove half base pairs from the bed files
+for f in `ls *bed`; do cat $f | cut -f 1-3 | sed 's/\.5//g' > tmp; mv tmp $f; done
+for f in `ls *bed`; do cat bed_head $f > tmp; mv tmp $f; done
+
+for f in `cat ../../local_ancestry/recent_hybrids.txt ../../local_ancestry/historical_hybrids.txt | sed 's/\r//g' `; do sed -e s/SAMPLE_NAME/$f/g run.01.get_indiv_vcf.sh > r.$f.sh; sbatch --mem=4G --nice r.$f.sh; echo $f; rm r.$f.sh; done
+mkdir indiv_vcfs
